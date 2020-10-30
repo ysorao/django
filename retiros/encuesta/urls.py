280bfd361875326b6883ab4c0d56d8e8665b9d95
yovanny sora <yovannysora@gmail.com>
2020-10-30 09:45:42 -0500

    moificacion

--- retiros/encuesta/urls.py
+++ retiros/encuesta/urls.py
@@ -4,6 +4,7 @@ from encuesta import views
 urlpatterns = [
     path('', views.inicio, name="Inicio"),
     path('encuesta', views.encuesta, name="Encuesta"),
-    path('pazysalvo', views.pazysalvo)
+    path('pazysalvo', views.pazysalvo),
+    path('result', views.result)
    
 ]
--- retiros/encuesta/views.py
+++ retiros/encuesta/views.py
@@ -7,9 +7,20 @@ def inicio(request):
     return render(request, "encuesta/encuesta.html")
 
 def encuesta(request):
-    return render(request, "encuesta/pazysalvo.html")
+    return render(request, "encuesta/encuesta.html")
 
 def pazysalvo(request):
     return render(request, "encuesta/pazysalvo.html")
 
+def result(request):
+    mensaje = "mensaje de respuesta"
+    nombre= 'Yovanny'
+    apellido='sora'
+    context={
+        'mensaje': mensaje,
+        'nombre': nombre,
+        'apellido': apellido,
+    }   
+
+    return render(request, "encuesta/result.html", context)
 
